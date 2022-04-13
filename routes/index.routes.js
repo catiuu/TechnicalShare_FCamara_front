cfcbb4ce8625823f459840bfbbdf96562671ce70
Angela Caldas <aes.caldas@gmail.com>
2022-04-13 16:43:01 -0300

    :sparkles: Validação front do login

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,23 +3,23 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Technical Share'})
+  res.render('index', {title: 'Orange Share'})
 })
 
 /* GET home page */
 router.get('/home', function (req, res, next) {
-  res.render('main', {title: 'Technical Share | Home', page: 'home'})
+  res.render('main', {title: 'Orange Share | Home', page: 'home'})
 })
 
 /* GET search page */
 router.get('/search', function (req, res, next) {
-  res.render('main', {title: 'Technical Share | Resultados', page: 'search'})
+  res.render('main', {title: 'Orange Share | Resultados', page: 'search'})
 })
 
 /* GET logged user page */
 router.get('/logged-user', function (req, res, next) {
   res.render('main', {
-    title: 'Technical Share | Seu Perfil',
+    title: 'Orange Share | Seu Perfil',
     page: 'logged-user'
   })
 })
