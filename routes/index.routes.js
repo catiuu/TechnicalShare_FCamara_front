df1a26df0860fc117ca85f1f5460f8a177073b3b
Angela Caldas <aes.caldas@gmail.com>
2022-04-13 12:26:22 -0300

    :recycle: Convertendo HTML em EJS

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Technical Share'})
+})
 
-module.exports = router;
+/* GET home page */
+router.get('/home', function (req, res, next) {
+  res.render('main', {title: 'Technical Share | Home', page: 'home'})
+})
+
+/* GET search page */
+router.get('/search', function (req, res, next) {
+  res.render('main', {title: 'Technical Share | Resultados', page: 'search'})
+})
+
+/* GET logged user page */
+router.get('/logged-user', function (req, res, next) {
+  res.render('main', {
+    title: 'Technical Share | Seu Perfil',
+    page: 'logged-user'
+  })
+})
+
+module.exports = router
