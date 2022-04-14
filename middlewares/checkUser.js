f8eedac02b6f575b2562aab15f4556fb7597fa15
Catiussia Nascimento <85588757+catiuu@users.noreply.github.com>
2022-04-13 21:28:43 -0300

    Revert "Anne estruturação"

--- middlewares/checkUser.js
+++
@@ -1,8 +0,0 @@
-const checkUser = (req, res, next) => {
-  if (req.idLogged != req.body.userId) {
-    return res.status(403).json({ message: "Acesso negado!" });
-  }
-  next();
-};
-
-module.exports = checkUser;
